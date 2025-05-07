f0a9a91b51bd4005bd9934b7c58b8afbf6bac256
Tidehunter123 <roland.ubanan20@gmail.com>
2025-05-07 16:15:38 -0700

    fix: correct the logic

--- src/services/jobsService.ts
+++ src/services/jobsService.ts
@@ -243,7 +243,7 @@ export const getJobsData = async (
     //     : "Early Career";
     // console.log("Job Type:", jobType);
 
-    const baseFilter = `NOT(OR({Status} = 'Not approved', {Status} = 'Archived'))`;
+    const baseFilter = `{Status} = 'Archived')`;
     const filterFormula = buildFilterFormula(`AND(${baseFilter}`, filters);
 
     allRecords = await base("Job Postings")
